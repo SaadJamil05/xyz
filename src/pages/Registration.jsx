ea4251ac9fdd9687cc523ca5808d337880b670d6
Muhammad Umar Yousaf <106830172+Umar792@users.noreply.github.com>
2024-01-30 02:21:33 +0500

    build add

--- src/pages/Registration.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Signup from "../Components/accounts/Signup";
-
-const Registration = () => {
-  return <Signup />;
-};
-
-export default Registration;
--- src/setting/GlobleVariable.js
+++
@@ -1,6 +0,0 @@
-// export const ApiURL = "http://192.168.1.166:4000/api";
-// export const ApiURL = "http://192.168.1.163:4000/api";
-// export const ApiMediaURL = "http://localhost:4000/uploads/";
-// ========================================= live
-export const ApiURL = "http://44.211.254.82/api";
-export const ApiMediaURL = "http://44.211.254.82/uploads/";
